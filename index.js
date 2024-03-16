9721eb12a082546041bac63eadbeb662698d0849
Sergio Salazar Williams <sawsdevx8@gmail.com>
2024-03-16 09:06:43 -0500

    Add blog list step 2

--- index.js
+++ index.js
@@ -1,16 +1,7 @@
-const express = require('express')
-const app = express()
-const cors = require('cors')
-const mongoose = require('mongoose')
 const config = require('./utils/config')
+const app = require('./app')
 
-const blogsRouter = require('./controllers/blogs')
 
-mongoose.connect(config.MONGODB_URI)
-
-app.use(cors())
-app.use(express.json())
-app.use('/api/blogs', blogsRouter)
 
 app.listen(config.PORT, () => {
   console.log(`Server running on port ${config.PORT}`)
