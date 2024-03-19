5acc90e60b5dd535b59891ca420a63381bbfacf4
Sergio Salazar Williams <sawsdevx8@gmail.com>
2024-03-19 07:08:01 -0500

    Add exercise 4.4
    Add total like function and tests

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -2,8 +2,20 @@ const dummy = (blogs) => {
     return 1
 }
 
+const totalLikes = (blogs) => {
+    if(blogs.length === 0)
+    return 0
+
+    if(blogs.length === 1)
+    return blogs[0].likes
+
+    const totalLikes = blogs.reduce((counter, post) => counter + Number(post.likes), 0 )
+    return totalLikes
+}
+
 
 
 module.exports = {
-    dummy
+    dummy,
+    totalLikes
 }
