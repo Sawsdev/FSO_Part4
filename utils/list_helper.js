fc35c361fbd45d7416c5c27389acbfa20da9be2f
Sergio Salazar Williams <sawsdevx8@gmail.com>
2024-03-19 07:32:32 -0500

    Add exercise 4.5
    Add favoriteblog function and tests

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -6,16 +6,29 @@ const totalLikes = (blogs) => {
     if(blogs.length === 0)
     return 0
 
-    if(blogs.length === 1)
-    return blogs[0].likes
 
     const totalLikes = blogs.reduce((counter, post) => counter + Number(post.likes), 0 )
     return totalLikes
+}
+
+const favoriteBlog = (blogs) => {
+    if(blogs.length === 0)
+    return false
+   
+    const maxblogsLikes = Math.max(...blogs.map((post) => post.likes) )
+    const maxLikesBlog = blogs.find((post) => post.likes === maxblogsLikes)
+    const favoriteBlog = {
+        title: maxLikesBlog.title,
+        author: maxLikesBlog.author,
+        likes: maxLikesBlog.likes
+    }
+    return favoriteBlog
 }
 
 
 
 module.exports = {
     dummy,
-    totalLikes
+    totalLikes,
+    favoriteBlog
 }
