47853d5f847815c82282f3753dad9fc7fcf5c76c
Sergio Salazar Williams <sawsdevx8@gmail.com>
2024-03-30 09:06:22 -0500

    Add exercise 4.9 Blog list tests step 2

--- tests/blog_api.test.js
+++ tests/blog_api.test.js
@@ -14,7 +14,21 @@ test('should list every blog post in database in JSON format', async () => {
         .expect('Content-Type',/application\/json/)
 });
 
+test('every blog post in the list must have the correct keys', {only: true}, async () => {
+    const response = await api.get('/api/blogs')
+    const blogKeys = {
+        id: "5a422a851b54a676234d17f7",
+        title: "React patterns",
+        author: "Michael Chan",
+        url: "https://reactpatterns.com/"
+    } 
+    response.body.map((blog) => {
+        assert.strictEqual(JSON.stringify(Object.keys(blogKeys).sort()), JSON.stringify(Object.keys(blog).sort()))
+    })
+});
+
 
 after(async () => {
     await mongoose.connection.close()
+    console.log("closed");
 })
