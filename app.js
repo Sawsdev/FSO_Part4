eb746dba49b986fee826c24f934b9593f03608f7
Sergio Salazar Williams <sawsdevx8@gmail.com>
2024-04-19 07:22:09 -0500

    Add Blog list expansion step 6
    Add login capabilities

--- app.js
+++ app.js
@@ -5,12 +5,14 @@ const cors = require('cors')
 const app = express()
 const blogsRouter = require('./controllers/blogs')
 const usersRouter = require('./controllers/users')
+const loginRouter = require('./controllers/login')
 
 
 mongoose.connect(config.MONGODB_URI)
 
 app.use(cors())
 app.use(express.json())
+app.use('/api/login', loginRouter)
 app.use('/api/blogs', blogsRouter)
 app.use('/api/users', usersRouter)
 
